a2981d06381cc571a18c56f01a6abd896222d3be
GromGT12 <m.krymov@wp.pl>
2023-07-11 11:45:58 +0200

    maks 11

--- src/main/java/Product/Service/ProductServiceImpl.java
+++ src/main/java/Product/Service/ProductServiceImpl.java
@@ -1,12 +1,15 @@
 package Product.Service;
+
 import Product.Controller.ProductDTO.ProductDTO;
 import Product.Controller.ProductDTO.ProductFilterDTO;
-
-
 import Product.Convertor.ProductConverter;
+import Product.Repository.PagingProductRepository;
 import Product.Repository.Product;
 import Product.Repository.SpringDataProductRepository;
 import Product.validator.ProductValidator;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,24 +22,29 @@ public class ProductServiceImpl implements ProductService {
     private final SpringDataProductRepository springDataProductRepository;
     private final ProductConverter productConverter;
     private final ProductValidator productValidator;
+    private final PagingProductRepository pagingProductRepository;
 
-    public ProductServiceImpl(SpringDataProductRepository springDataProductRepository, ProductConverter productConverter, ProductValidator productValidator) {
+    public ProductServiceImpl(SpringDataProductRepository springDataProductRepository, ProductConverter productConverter, ProductValidator productValidator, PagingProductRepository pagingProductRepository) {
         this.springDataProductRepository = springDataProductRepository;
         this.productConverter = productConverter;
         this.productValidator = productValidator;
+        this.pagingProductRepository = pagingProductRepository;
     }
+
     @Override
     @Transactional(readOnly = true)
     public List<ProductDTO> getAllProducts() {
         Collection<Product> all = springDataProductRepository.findAll();
         return productConverter.convertToDto(all);
     }
+
     @Override
     @Transactional(readOnly = true)
     public ProductDTO getById(Integer id) {
-        Product product =  springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
+        Product product = springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
         return productConverter.convertToDto(product);
     }
+
     @Override
     @Transactional
     public Integer createProduct(ProductDTO productToCreate) {
@@ -45,10 +53,11 @@ public class ProductServiceImpl implements ProductService {
         Product saveProduct = springDataProductRepository.save(product);
         return saveProduct.getId();
     }
+
     @Override
     @Transactional
     public void deleteProduct(Integer id) {
-        Product product =  springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
+        Product product = springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
         springDataProductRepository.delete(product);
 
     }
@@ -63,7 +72,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDTO updateProduct(Integer id, ProductDTO productToUpdate) {
-        Product product =  springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
+        Product product = springDataProductRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Product not found" + id));
         Product entityToUpdate = productConverter.convertToEntity(productToUpdate);
         entityToUpdate.setId(id);
         Product updateEntity = springDataProductRepository.save(entityToUpdate);
@@ -75,5 +84,13 @@ public class ProductServiceImpl implements ProductService {
     public List<ProductDTO> search(ProductFilterDTO filter) {
         List<Product> allByNameAndDescription = springDataProductRepository.findAllByNameAndDescription(filter.getName(), filter.getDescription());
         return productConverter.convertToDto(allByNameAndDescription);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> getPage(Pageable pageable) {
+        Page<Product> productPage = pagingProductRepository.findAll(pageable);
+        List<ProductDTO> productDTOS = productConverter.convertToDto(productPage.getContent());
+        return new PageImpl<>(productDTOS, productPage.getPageable(), productPage.getTotalPages());
     }
 }
